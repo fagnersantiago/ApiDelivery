3a26c8f0605dff8e2cb27a196cc3a3d9a5c4ca06
Fagner Santiago <fagner.santiago@hotmail.com>
2023-01-17 17:49:00 -0300

    add prisma repository in deliveries

--- src/modules/deliveries/usecase/prisma/createDeliveriesPrisama.ts
+++ src/modules/deliveries/usecase/prisma/createDeliveriesPrisama.ts
@@ -2,8 +2,8 @@ import { prisma } from "../../../../databases/prismaClient";
 import { CreateDeliveryDTO } from "../../dto/createDeliveryDTO";
 
 export class CreateDeliveries {
-  execute({ id_client, item_name, created_at }: CreateDeliveryDTO) {
-    const deliveries = prisma.deliveries.create({
+  async execute({ id_client, item_name, created_at }: CreateDeliveryDTO) {
+    const deliveries = await prisma.deliveries.create({
       data: {
         id_client,
         item_name,
