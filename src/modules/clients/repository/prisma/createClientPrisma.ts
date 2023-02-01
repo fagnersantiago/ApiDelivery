7a912b6c811da925b0657378e28698c591d47113
Fagner Santiago <fagner.santiago@hotmail.com>
2023-02-01 18:30:23 -0300

    initial teste findAllDeliveiesByClient

--- src/modules/clients/repository/prisma/createClientPrisma.ts
+++ src/modules/clients/repository/prisma/createClientPrisma.ts
@@ -26,4 +26,17 @@ export class ClientPrismaRepository implements IClientRepository {
 
     return client;
   }
+
+  async findAllDeliveries(id_client: string) {
+    return await prisma.clients.findMany({
+      where: {
+        id: id_client,
+      },
+      select: {
+        username: true,
+        id: true,
+        deliveries: true,
+      },
+    });
+  }
 }
